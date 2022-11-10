1ff2abf0dead2e8f8fc1c0efa013abcbc5147b0f
Vancomycin <2112484232@qq.com>
2022-11-11 06:08:07 +0800

    jjj

--- recorder_pi/log_generate.py
+++ recorder_pi/log_generate.py
@@ -1,15 +1,27 @@
 #!/bin/python3
 import logging
+class CameraException(Exception):
+    def __init__(self,msg):
+        super(CameraException, self).__init__(msg)
+        self.__str__ = msg
 
 
 if __name__ == "__main__":
-    logging.critical('Hwloooo')
+    #logging.critical('Hwloooo')
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %a %H:%M:%S',
                         filename='../log/test.log',
-                        filemode='w')
+                        filemode='a')
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)-12s: [%(levelname)-8s] %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
 
     logging.debug('This is debug message')
     logging.info('This is info message')
     logging.warning('This is warning message')
+    logging.critical("Programme Stopped!")
